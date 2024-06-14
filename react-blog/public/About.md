906503d43dadd3838b044c6db5e0dedac5b77cb3
Tatsuohggggglaptop <morin10050@gmail.com>
2024-06-14 13:40:48 +0900

    Add RenderPage.js

---
+++ react-blog/public/About.md
@@ -0,0 +1,15 @@
+# About
+ゲーマーの個人ブログ。<br>
+あそんだゲームの感想やゲーム制作の進捗記事を不定期に更新。<br>
+このサイトの目的はひとえに、"好き"の共有です。
+
+<h1 className="includemargintoph1" >寝たほうがいい人の紹介</h1>
+<div className="d-flex flex-row align-start">
+<img src="monad.png"></img>
+<div className="d-flex flex-column align-start">
+
+## たつお
+<p>ゲームが好きです。<br>
+ロングスリーパーは常に寝不足。</p>
+</div>
+</div>
---
+++ react-blog/src/PixelArt.js
@@ -0,0 +1,40 @@
+import { Container } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
+import { useEffect,useState } from 'react';
+import './Body.css';
+import './pixelArt.css';
+
+export const PixelArt = () => {
+    return (
+        <Container fluid className='overflow-auto text-left parent' style={{width: "40%",height: "80vh",marginLeft: "0px" }}>
+            <div>
+                <header className='d-flex flex-row align-items-end'>
+                    <h1 style={{ color: "white" }}>ドット絵</h1>
+                    <p style={{ fontSize: "14px",marginBottom: "8px",marginLeft:"5px" }}>気ままにお絵描き</p>
+                </header>
+                <Text />
+            </div>
+        </Container>
+    );
+};
+
+const Text = () => {
+    const [markdown, setMarkdown] = useState("");
+
+    useEffect(() => {
+        fetch(`${process.env.PUBLIC_URL}/pixelart.md`)
+        .then((m) => {
+            return m.text();
+        })
+        .then((md) => {
+            setMarkdown(md);
+        });
+    }, []);
+
+    return (<ReactMarkdown components={components}>{markdown}</ReactMarkdown>)
+}
+
+const components = {
+    h2: ({ ...props}) => <h2 className='title ' style={{color: "white",marginBottom:"20px",borderBottom: "2px solid white"}}>{props.children}</h2>,
+    img: ({ ...props }) => <img className="img-fluid" style={{width: "100%", height: "100%"}} src={`${process.env.PUBLIC_URL}/${props.src}`} alt="aaa">{props.children}</img>,
+};
